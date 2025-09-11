ae7cda6a29051761cf27b7fe08bb10adab1f745b
Daniel Schmitz <daniel.schmitz@bayoo.net>
2025-09-11 18:58:24 +0200

    feat: init project

---
+++ nuxt.config.ts
@@ -0,0 +1,15 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: ['@nuxt/eslint', '@nuxt/image', '@nuxt/ui'],
+  devtools: { enabled: true },
+  css: ['~/assets/css/main.css'],
+  compatibilityDate: '2025-07-15',
+  eslint: {
+    config: {
+      standalone: false,
+      nuxt: {
+        sortConfigKeys: true,
+      },
+    },
+  },
+})
