9249f87b487e9e6d78acda7bca907381a1ab65c2
Daniel Schmitz <daniel.schmitz@bayoo.net>
2025-11-25 15:56:16 +0100

    chore: enable experimental settings

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ['@nuxt/eslint', '@nuxt/image', '@nuxt/ui'],
-  devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
+  ui: {
+    experimental: {
+      componentDetection: true,
+    },
+  },
+  experimental: {
+    inlineRouteRules: true,
+    typescriptPlugin: true,
+    extractAsyncDataHandlers: true,
+  },
   compatibilityDate: '2025-07-15',
   typescript: {
     typeCheck: true,
